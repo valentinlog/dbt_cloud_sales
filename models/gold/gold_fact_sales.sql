968fcff7f4ef15660580200fb663dd662b6d4fa1
Valentin Loghin <valentin.loghin@gmail.com>
2025-04-19 18:43:52 +0000

    created Gold layer

---
+++ models/gold/gold_fact_sales.sql
@@ -0,0 +1,11 @@
+select
+        sales_order_number,
+        CAST(order_date AS DATE) order_date,
+        product_key as product_key,
+        CAST(sales_territory_key AS VARCHAR(255)),
+        order_quantity,
+        unit_price,
+        sales_amount,
+        total_product_cost,
+        CAST(created_at AS DATE) as date_inserted
+from   {{ ref('silver_silver') }}
--- models/silver/silver_region.sql
+++ models/silver/silver_region.sql
@@ -2,5 +2,6 @@ select
    SalesTerritoryKey as sales_territory_key,
    Region as region, 
    Group as group,
+   country,
    current_timestamp as created_at
 from   {{ ref('bronze_region') }}
